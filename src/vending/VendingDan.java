23c51dce684968787fcae424a701a456bdf8cbd4
DanielleHall <halldan4@yahoo.com>
2016-04-20 16:19:51 -0400

    updated array

--- src/vending/VendingDan.java
+++ src/vending/VendingDan.java
@@ -1,34 +1,14 @@
 package vending;
 import java.io.*;
 	import java.util.Scanner;
-public class VendingDani {
+public class VendingDan {
 
-		private int price;
-		private int qty;
 		
-		double[][] product = new double[price][qty];
-	    product[0][0] = .60;
-	    product[0][1] = 1.00;
-	    product[0][2] = .95;
-	    product[0][3] = .95;
-	    product[0][4] = 2.00;
-	    product [0][5] = .50;
-	    product [0][6]= .50;
-	    product [0][7]= .75;
-	    product [0][8]= 1.75;
-	    
-	    product[1][0] = 5;
-	    product[1][1] = 5;
-	    product[1][2] = 5;
-	    product[1][3] = 5;
-	    product[1][4] = 5;
-	    product [1][5] = 5;
-	    product [1][6]= 5;
-	    product [1][7]= 5;
-	    product [1][8]= 5;
+		double[] qty = {5, 5, 5, 5, 5, 5, 5, 5, 5};
+	    double[] price = {.50, .75, .95, .95, 2.50, 1.85, .65, .80, .90};
 	    
 	}
-}
+
 
 
 		
