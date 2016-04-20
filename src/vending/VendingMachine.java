2718e34dbf4612f4ccdfacd6297c5e0f8b460b1e
DanielleHall <halldan4@yahoo.com>
2016-04-20 15:47:03 -0400

    updated array

--- src/vending/VendingMachine.java
+++ src/vending/VendingMachine.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 
 public class VendingMachine {
 
-//data members
-private Item[] stock;  //Array of Item objects in machine
 
-public VendingMachine(String filename) throws FileNotFoundException{
-    //Open the file to read with the scanner
-    File file = new File(filename);
-    Scanner scan = new Scanner(file);
-}
-}
+	
+	double[][] product = new double[][];
+    product[0][0] = .60;
+    product[0][1] = 1.00;
+    product[0][2] = .95;
+    product[0][3] = .95;
+    product[0][4] = 2.00;
+    product [0][5] = .50;
+    product [0][6]= .50;
+    product [0][7]= .75;
+    product [0][8]= 1.75;
+    
+    product[1][0] = 5;
+    product[1][1] = 5;
+    product[1][2] = 5;
+    product[1][3] = 5;
+    product[1][4] = 5;
+    product [1][5] = 5;
+    product [1][6]= 5;
+    product [1][7]= 5;
+    product [1][8]= 5;
+    
+}}
+
