ea3db375789ec18438ad7cfa6edd6d634991eed1
Nick Fenton <njamesfenton@gmail.com>
2016-04-20 16:38:27 -0400

    building the main

--- src/vending/RunMachine.java
+++ src/vending/RunMachine.java
@@ -6,8 +6,25 @@ public class RunMachine {
 		
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to the DNG Vending Machine");
+		System.out.println("Welcome to the DNG Vending Machine would here are the items for sale!");
 		ProductIO.readfromFile();
+		System.out.println("Would you like to buy item(1) or learn more about an item(2)");
+		int input = 0;
+		input = sc.nextInt();
+		sc.nextLine();
+		
+		switch (input)
+		{
+			case 1:			
+				
+				break;
+		
+		    case 2:
+		    	
+		    	
+		    	break;
+		    	
+		    }
 		
 		
 		
