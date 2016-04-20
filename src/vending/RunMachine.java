286c2b1137e04df83def9c912d2a05aed27b0b7b
DanielleHall <halldan4@yahoo.com>
2016-04-20 18:49:48 -0400

    corrected spelling in welcome message

--- src/vending/RunMachine.java
+++ src/vending/RunMachine.java
@@ -6,9 +6,9 @@ public class RunMachine {
 		
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to the DNG Vending Machine would here are the items for sale!");
+		System.out.println("Welcome to the DNG Vending Machine! Here are the items for sale!");
 		ProductIO.readfromFile();
-		System.out.println("Would you like to learm more about these items(1) or purcahse an item(2)");
+		System.out.println("Would you like to learn more about these items(1) or purchase an item(2)");
 		int input = 0;
 		input = Validator.getValidInt1(1, 2);
 		sc.nextLine();
