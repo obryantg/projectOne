d34dbf5d936a6a9af8c4251e15eb4a7f28016acd
DanielleHall <halldan4@yahoo.com>
2016-04-21 10:22:45 -0400

    updated the continue y/n validator

--- src/vending/Validator.java
+++ src/vending/Validator.java
@@ -33,7 +33,7 @@ public class Validator {
             if (s.compareTo("Y") == 0 || s.compareTo("y") == 0 || s.compareTo("N") == 0 || s.compareTo("n") == 0)
             isValid = true;
             else
-            System.out.println("\nError! Entry must be 'y' or 'n'. Try again.");
+            System.out.println("\nError! You must enter 'y' or 'n'!");
        }
 
             return s;
